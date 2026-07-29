83151baf30b1c0adaac5feedc17c06a1066bb5f5
Tharun C <tharungowda1905@gmail.com>
2026-07-29 15:48:26 +0530

    Update CardioPulseApplication.java

--- src/main/java/com/ece/dsp/CardioPulseApplication.java
+++ src/main/java/com/ece/dsp/CardioPulseApplication.java
@@ -1,1 +1,11 @@
+package com.ece.dsp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioPulseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CardioPulseApplication.class, args);
+    }
+}
